9009b18a85a98e1ac115785cb5afb1c80e93e53f
Meirtz <593865730@qq.com>
2014-02-04 21:06:20 +0800

    Monsoon_MainDynamicLibrary
    A nice chat tool

---
+++ Monsoon_MainDynamicLibrary/Monsoon/Monsoon.h
@@ -0,0 +1,16 @@
+//
+//  Monsoon.h
+//  Monsoon
+//
+//  Created by Meirtz on 14-2-3.
+//  Copyright (c) 2014年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface Monsoon : NSObject
+
+- (void)patchedLaunch:(_Bool)arg1;
+
+@end
---
+++ Monsoon_MainDynamicLibrary/Monsoon/Monsoon.m
@@ -0,0 +1,50 @@
+//
+//  Monsoon.m
+//  Monsoon
+//
+//  Created by Meirtz on 14-2-3.
+//  Copyright (c) 2014年 __MyCompanyName__. All rights reserved.
+//
+
+#import "Monsoon.h"
+
+
+#include <stdio.h>
+#include <objc/runtime.h>
+#import <Foundation/Foundation.h>
+
+
+@interface SBAppSliderController : UIViewController
+{
+    NSMutableArray *_appList;
+}
+
+- (NSArray *)applicationList;
+- (void)_quitAppAtIndex:(unsigned int)arg1;
+- (void)forceDismissAnimated:(BOOL)arg1;
+
+@end
+
+static IMP sOriginalImp = NULL;
+
+
+@implementation Monsoon
+
+
++ (void)load{
+    Class originalClass = NSClassFromString(@"SBAppSliderController");
+    Method originalMeth = class_getInstanceMethod(originalClass, @selector(switcherWasPresented:));
+    sOriginalImp = method_getImplementation(originalMeth);
+    
+	Method replacementMeth = class_getInstanceMethod(NSClassFromString(@"Monsoon"), @selector(patchedLaunch:));
+	method_exchangeImplementations(originalMeth, replacementMeth);
+    
+}
+
+- (void)patchedLaunch:(_Bool)arg1{
+    sOriginalImp(self, @selector(switcherWasPresented:), self);
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"INJECTED" message:@"Method has been replaced by objc_runtime dynamic library\nDYLD_INSERT_LIBRARIES=libMonsoon.dylib" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil];
+    
+    [alert show];
+}
+@end
